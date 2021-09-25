edbad5878c4e01c635bf16af52d70cbefbc725f2
NIGHT LIGHT <jaredinyaagha@gmail.com>
2021-09-24 21:23:32 -0400

    Switched from checkboxes to prompts for easier dev

--- script.js
+++ script.js
@@ -18,28 +18,34 @@ function writePassword() {
   shuffle();
 }
 
-function generatePassword(upperCase, lowerCase) {
-  let password = '';
-  for (var i=0; i< 10; i++) {
-    password += upperCase(Math.floor(Math.random() * upperCase.length))};
-    console.log(password);
+function generatePassword() {
+  let parameters = '';
+  var passwordLength = prompt("Input Desired Password Length", "(Must be in between 8 and 128 characters long): ")
+  if ( passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength) ) {
+    alert("Number must be in between 8 and 128!");
+  }
+
+  
+  // for (var i=0; i< 10; i++) {
+  //   password += upperCase(Math.floor(Math.random() * upperCase.length))};
+  //   console.log(password);
 }
 
-function shuffle(upperCase, lowerCase) {
-  let password = '';
-  for (var i=0; i< 10; i++) {
-    password += upperCase(Math.floor(Math.random() * upperCase.length))};
-    console.log(password);
-  }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// function shuffle(upperCase, lowerCase) {
+//   let password = '';
+//   for (var i=0; i< 10; i++) {
+//     password += upperCase(Math.floor(Math.random() * upperCase.length))};
+//     console.log(password);
+//   }
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
 
-  // console.log("Hey Toni lol")
-  // if (upperCaseChecked.checked) {
-  // console.log(upperCase.sort(() => 5 + Math.random()))
-  // } 
-  if (upperCaseChecked && lowerCaseChecked && specialCharChecked && numericChecked == true ) {
-    const passArray = [upperCase + lowerCase + specialChar + numeric]
-  }
+//   // console.log("Hey Toni lol")
+//   // if (upperCaseChecked.checked) {
+//   // console.log(upperCase.sort(() => 5 + Math.random()))
+//   // } 
+//   if (upperCaseChecked && lowerCaseChecked && specialCharChecked && numericChecked == true ) {
+//     const passArray = [upperCase + lowerCase + specialChar + numeric]
+//   }
 
-  // else if ()
+//   // else if ()
