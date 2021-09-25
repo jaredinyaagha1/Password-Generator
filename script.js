47edf2ec569bbd4580f6b62290061fd542f4a8ed
NIGHT LIGHT <jaredinyaagha@gmail.com>
2021-09-24 20:29:12 -0400

    consolidating arrays and commenting out extra QS

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // variable initialization
 var generateBtn = document.querySelector("#generate");
-const upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
-const lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
-const specialChar = [' ','!','"','#','$','%','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~']
-const numeric = ['1','2','3','4','5','6','7','8','9','0']
-const upperCaseChecked = document.querySelector("#upperCase");
-const lowerCaseChecked = document.querySelector("#lowerCase");
-const numericChecked = document.querySelector("#numeric");
-const specialCharChecked = document.querySelector("#SpecialChar");
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+const specialChar = "!#$%&'()*+,-./:;<=>?@][^_`{|}~"
+const numeric = '1234567890'
+// const upperCaseChecked = document.querySelector("#upperCase");
+// const lowerCaseChecked = document.querySelector("#lowerCase");
+// const numericChecked = document.querySelector("#numeric");
+// const specialCharChecked = document.querySelector("#SpecialChar");
 
 // Write password to the #password input
 function writePassword() {
